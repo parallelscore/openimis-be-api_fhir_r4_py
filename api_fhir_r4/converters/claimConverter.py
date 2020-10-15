f311e40a8b777944dc99fade89906f07dba470ea
Patrick Delcroix <patrick.delcroix@swisstph.ch>
2020-10-15 09:22:59 +0200

    fixing claim resource

--- api_fhir_r4/converters/claimConverter.py
+++ api_fhir_r4/converters/claimConverter.py
@@ -28,16 +28,16 @@ class ClaimConverter(BaseFHIRConverter, ReferenceConverterMixin):
         fhir_claim.created = imis_claim.date_claimed.isoformat()
         if imis_claim.health_facility is None:
             raise FHIRRequestProcessException(['Cannot construct a %s claim if HF is None' % (imis_claim.uuid) ])
-        fhir_claim.facility = HealthcareServiceConverter.build_fhir_resource_reference(imis_claim.health_facility,imis_claim.health_facility.code )
+        fhir_claim.facility = HealthcareServiceConverter.build_fhir_resource_reference(imis_claim.health_facility,'Location', imis_claim.health_facility.code )
         cls.build_fhir_identifiers(fhir_claim, imis_claim)
         if imis_claim.insuree is  None:
             raise FHIRRequestProcessException(['Cannot construct a %s claim if Insuree is None' % (imis_claim.uuid)] )
-        fhir_claim.patient = PatientConverter.build_fhir_resource_reference(imis_claim.insuree, imis_claim.insuree.chf_id)
+        fhir_claim.patient = PatientConverter.build_fhir_resource_reference(imis_claim.insuree, 'Patient', imis_claim.insuree.chf_id)
         cls.build_fhir_billable_period(fhir_claim, imis_claim)
         cls.build_fhir_diagnoses(fhir_claim, imis_claim)
         cls.build_fhir_total(fhir_claim, imis_claim)
         if imis_claim.admin is not None:
-            fhir_claim.enterer = PractitionerConverter.build_fhir_resource_reference(imis_claim.admin)
+            fhir_claim.enterer = PractitionerConverter.build_fhir_resource_reference(imis_claim.admin, 'Practitioner')
         #else:
         #    raise FHIRRequestProcessException(['Cannot construct a %s claim if claim admin is None' %(imis_claim.uuid)] )
         cls.build_fhir_type(fhir_claim, imis_claim)
