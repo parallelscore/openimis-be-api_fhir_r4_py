14aee94b6c67123e7285406313e26582d7d01c6a
Damian Borowiecki <dborowiecki@soldevelo.com>
2020-11-24 15:39:34 +0100

    Improve PractitionerRole view

--- api_fhir_r4/views.py
+++ api_fhir_r4/views.py
@@ -124,10 +124,18 @@ class PractitionerRoleViewSet(BaseFHIRView, viewsets.ModelViewSet):
     lookup_field = 'uuid'
     serializer_class = PractitionerRoleSerializer
     #permission_classes = (FHIRApiPractitionerPermissions,)
+
     def list(self, request, *args, **kwargs):
+        identifier = request.GET.get("identifier")
         queryset = self.get_queryset()
         if identifier:
             queryset = queryset.filter(code=identifier)
+        else:
+            queryset = queryset.filter(validity_to__isnull=True).order_by('validity_from'
+                                                                          )
+        serializer = PractitionerRoleSerializer(self.paginate_queryset(queryset), many=True)
+        return self.get_paginated_response(serializer.data)
+
     def perform_destroy(self, instance):
         instance.health_facility_id = None
         instance.save()
