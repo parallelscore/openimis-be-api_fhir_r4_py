ec9c5cd3633c1ca137a26da197118cd28c3e2a56
sniedzielski <sniedzielski@soldevelo.com>
2021-07-13 11:52:58 +0200

    OE0-18: deleted unused models

--- api_fhir_r4/models/bankAccount.py
+++
@@ -1,8 +0,0 @@
-# TODO check this as a part of extension model
-"""
-from api_fhir_r4.models import Element, Property
-
-class BankAccount(Element):
-    bank = Property('bank', str)  # NameUse
-    no = Property('no', str)
-"""
