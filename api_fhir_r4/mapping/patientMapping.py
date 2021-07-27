4e78145664c9c216c4b2e97cf1461a13ebc2ed09
sniedzielski <sniedzielski@soldevelo.com>
2021-07-27 11:48:24 +0200

    OE0-32: added new extensions, update existing and remove not used in profile

--- api_fhir_r4/mapping/patientMapping.py
+++ api_fhir_r4/mapping/patientMapping.py
@@ -11,3 +11,36 @@ class RelationshipMapping(object):
         "7": "Others",
         "8": "Spouse",
     }
+
+
+class EducationLevelMapping(object):
+
+    education_level = {
+        "1": "Nursery",
+        "2": "Primary school",
+        "3": "Secondary school",
+        "4": "University",
+        "5": "Postgraduate studies",
+        "6": "PHD",
+        "7": "Other",
+    }
+
+
+class PatientProfessionMapping(object):
+
+    patient_profession = {
+        "1": "Housewife",
+        "2": "Employee",
+        "3": "Self Employee",
+        "4": "Other",
+    }
+
+
+class IdentificationTypeMapping(object):
+
+    identification_type = {
+        "D": "Driver's License",
+        "N": "National ID",
+        "P": "Passport",
+        "V": "Voter Card",
+    }
