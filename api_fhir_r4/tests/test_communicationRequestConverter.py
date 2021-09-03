f2c1725ed40c0d5216753f1c05f87c1f8d54a0ad
sniedzielski <sniedzielski@soldevelo.com>
2021-09-03 16:11:11 +0200

    OE0-29: fixed naming of json_dict

--- api_fhir_r4/tests/test_communicationRequestConverter.py
+++ api_fhir_r4/tests/test_communicationRequestConverter.py
@@ -13,10 +13,10 @@ class CommunicationRequestConverterTestCase(CommunicationRequestTestMixin):
     def setUp(self):
         super(CommunicationRequestConverterTestCase, self).setUp()
         dir_path = os.path.dirname(os.path.realpath(__file__))
-        self._test_claim_response_json_representation = open(dir_path + self.__TEST_COMMUNICATION_REQUEST_JSON_PATH).read()
+        self._test_communication_request_json_representation = open(dir_path + self.__TEST_COMMUNICATION_REQUEST_JSON_PATH).read()
         # .dumps() will not put a newline at the end of the "file" but editors will
-        if self._test_claim_response_json_representation[-1:] == "\n":
-            self._test_claim_response_json_representation = self._test_claim_response_json_representation[:-1]
+        if self._test_communication_request_json_representation[-1:] == "\n":
+            self._test_communication_request_json_representation = self._test_communication_request_json_representation[:-1]
 
     def test_to_fhir_obj(self):
         imis_feedback = self.create_test_imis_instance()
@@ -25,6 +25,6 @@ class CommunicationRequestConverterTestCase(CommunicationRequestTestMixin):
 
     def test_fhir_object_to_json_request(self):
         self.setUp()
-        dict_communication_request = json.loads(self._test_claim_response_json_representation)
+        dict_communication_request = json.loads(self._test_communication_request_json_representation)
         fhir_communication_request = CommunicationRequest(**dict_communication_request)
         self.verify_fhir_instance(fhir_communication_request)
