8f3320a56b4702d844db03c624cf0f364dbeca37
Patrick Delcroix <patrick.delcroix@swisstph.ch>
2021-09-10 14:51:52 +0200

    call fct iso const

--- api_fhir_r4/configurations/generalConfiguration.py
+++ api_fhir_r4/configurations/generalConfiguration.py
@@ -60,6 +60,6 @@ class GeneralConfiguration(BaseConfiguration):
     @classmethod
     def get_system_base_url(cls):
         if SITE_ROOT is not None:       
-            return join(cls.get_config_attribute("base_url"),'/',settings.SITE_ROOT)
+            return join(cls.get_config_attribute("base_url"),'/',settings.SITE_ROOT())
         else:
             return cls.get_config_attribute("base_url")
