4ad36c170d3b9506ba2c857b76106dbc6ba15515
Patrick Delcroix <patrick.delcroix@swisstph.ch>
2021-09-10 11:21:51 +0200

    fix import not found

--- api_fhir_r4/configurations/generalConfiguration.py
+++ api_fhir_r4/configurations/generalConfiguration.py
@@ -1,6 +1,6 @@
 from api_fhir_r4.configurations import BaseConfiguration
 from api_fhir_r4.defaultConfig import DEFAULT_CFG
-from django.conf.settings import SITE_ROOT
+from core.conf import settings
 
 
 class GeneralConfiguration(BaseConfiguration):
@@ -60,6 +60,6 @@ class GeneralConfiguration(BaseConfiguration):
     @classmethod
     def get_system_base_url(cls):
         if SITE_ROOT is not None:       
-            return join(cls.get_config_attribute("base_url"),'/',SITE_ROOT)
+            return join(cls.get_config_attribute("base_url"),'/',settings.SITE_ROOT)
         else:
             return cls.get_config_attribute("base_url")
