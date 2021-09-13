b2ecef11b17a5f02d9a2401f0b4255e775cda607
sniedzielski <sniedzielski@soldevelo.com>
2021-09-13 14:00:08 +0200

    OE0-55: integration tests for CodeSystem endpoints

--- api_fhir_r4/views/fhir/code_systems/patient_education_level.py
+++ api_fhir_r4/views/fhir/code_systems/patient_education_level.py
@@ -3,9 +3,11 @@ from api_fhir_r4.serializers import CodeSystemSerializer
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from api_fhir_r4.permissions import FHIRApiInsureePermissions
 
 from rest_framework.views import APIView
 from api_fhir_r4.views import CsrfExemptSessionAuthentication
+from django.core.exceptions import PermissionDenied
 
 
 class CodeSystemOpenIMISPatientEducationLevelViewSet(viewsets.ViewSet):
@@ -16,6 +18,8 @@ class CodeSystemOpenIMISPatientEducationLevelViewSet(viewsets.ViewSet):
 
     def list(self, request):
         # we don't use typical instance, we only indicate the model and the field to be mapped into CodeSystem
+        if not request.user.has_perms(FHIRApiInsureePermissions.permissions_get):
+            raise PermissionDenied("unauthorized")
         serializer = CodeSystemSerializer(
             instance=None,
             **{
