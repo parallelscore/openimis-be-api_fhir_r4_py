a2fd95585e2d274382825e0876dab7c4e375a596
sniedzielski <sniedzielski@soldevelo.com>
2021-10-04 16:32:34 +0200

    OE0-74: expired status map to active

--- api_fhir_r4/mapping/coverageMapping.py
+++ api_fhir_r4/mapping/coverageMapping.py
@@ -9,7 +9,7 @@ class CoverageStatus(object):
             1: R4CoverageConfig.get_status_idle_code(),
             2: R4CoverageConfig.get_status_active_code(),
             4: R4CoverageConfig.get_status_suspended_code(),
-            8: R4CoverageConfig.get_status_suspended_code(),
+            8: R4CoverageConfig.get_status_active_code(),
             None: R4CoverageConfig.get_status_idle_code(),
         }
         return codes[code]
