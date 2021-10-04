1a9e50c44613c61f0f870ee7e4d39936168236f8
sniedzielski <sniedzielski@soldevelo.com>
2021-10-04 19:36:14 +0200

    OE0-66: added POST test for claim, updated mapping in coverage

--- api_fhir_r4/mapping/coverageMapping.py
+++ api_fhir_r4/mapping/coverageMapping.py
@@ -6,10 +6,10 @@ class CoverageStatus(object):
     @classmethod
     def map_status(cls, code):
         codes = {
-            1: R4CoverageConfig.get_status_idle_code(),
+            1: R4CoverageConfig.get_status_draft_code(),
             2: R4CoverageConfig.get_status_active_code(),
-            4: R4CoverageConfig.get_status_suspended_code(),
+            4: R4CoverageConfig.get_status_cancelled_code(),
             8: R4CoverageConfig.get_status_active_code(),
-            None: R4CoverageConfig.get_status_idle_code(),
+            None: R4CoverageConfig.get_status_draft_code(),
         }
         return codes[code]
