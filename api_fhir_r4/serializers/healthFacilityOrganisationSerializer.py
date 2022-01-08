8993fd22061449adf0dde04b6cfb1d91fef786c0
Damian Borowiecki <dborowiecki@soldevelo.com>
2022-01-08 12:28:46 +0100

    OAI-61: HF can be created using Organization Serialzier

--- api_fhir_r4/serializers/healthFacilityOrganisationSerializer.py
+++ api_fhir_r4/serializers/healthFacilityOrganisationSerializer.py
@@ -1,13 +1,33 @@
+import copy
+
+from location.gql_mutations import update_or_create_health_facility
+
 from api_fhir_r4.converters import HealthFacilityOrganisationConverter
 from api_fhir_r4.serializers import BaseFHIRSerializer
-from location.models import HealthFacility
 
 
 class HealthFacilityOrganisationSerializer(BaseFHIRSerializer):
     fhirConverter = HealthFacilityOrganisationConverter()
 
     def create(self, validated_data):
-        raise NotImplementedError('Health Facility should be created using Practitioner resource.')
+        data = copy.deepcopy(validated_data)
+        return self.__create_or_update(data)
 
     def update(self, instance, validated_data):
-        raise NotImplementedError('Health Facility should be updated using Practitioner resource.')
+        validated_data['id'] = instance.id
+        validated_data['uuid'] = instance.uuid
+
+        data = copy.deepcopy(validated_data)
+        return self.__create_or_update(data)
+
+    def __create_or_update(self, validated_data):
+        request = self.context.get("request")
+        user = request.user
+        data = copy.deepcopy(validated_data)
+        if '_state' in validated_data:
+            del data['_state']
+
+        if not data.get('offline', None):
+            data['offline'] = False
+
+        return update_or_create_health_facility(data, user)
