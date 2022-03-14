7e9e50efd463f5059e1eee30b952359195f363ff
user <kmalinowski@soldevelo.com>
2022-03-14 12:03:27 +0100

    OPL-58 Updated exceptions in serializer, fixed url parsing

--- api_fhir_r4/validation.py
+++ api_fhir_r4/validation.py
@@ -1,31 +1,20 @@
-from typing import Type
-
 from django.core.exceptions import ValidationError
 
 from api_fhir_r4.models import Subscription
-from core.models import HistoryModel
 from core.validation import BaseModelValidation
 
 
 class SubscriptionValidation(BaseModelValidation):
     OBJECT_TYPE = Subscription
 
-    allowed_resources = ('patient', 'invoice')
-
     @classmethod
     def validate_create(cls, user, **data):
-        cls.check_allowed_resources(**data)
+        super().validate_create(user, **data)
 
     @classmethod
     def validate_update(cls, user, **data):
-        cls.check_allowed_resources(**data)
+        super().validate_update(user, **data)
 
     @classmethod
     def validate_delete(cls, user, **data):
         super().validate_delete(user, **data)
-
-    @classmethod
-    def check_allowed_resources(cls, **data):
-        resource_type = data.get('criteria', {}).get('resource_type', '').lower()
-        if not resource_type or resource_type not in cls.allowed_resources:
-            raise ValidationError(f'Resource type not allowed: {resource_type}')
--- api_fhir_r4/views/fhir/subscription.py
+++ api_fhir_r4/views/fhir/subscription.py
@@ -1,6 +1,6 @@
+from rest_framework.exceptions import PermissionDenied, APIException
 from rest_framework.viewsets import ModelViewSet
 
-from api_fhir_r4.exceptions import FHIRException
 from api_fhir_r4.models import Subscription
 from api_fhir_r4.permissions import FHIRApiSubscriptionPermissions
 from api_fhir_r4.serializers import SubscriptionSerializer
@@ -9,14 +9,16 @@ from api_fhir_r4.views.fhir.base import BaseFHIRView
 
 
 class SubscriptionViewSet(BaseFHIRView, ModelViewSet):
-    _error_while_saving = 'Error while saving a subscription: %(msg)s'
+    _error_while_deleting = 'Error while deleting a subscription: %(msg)s'
     serializer_class = SubscriptionSerializer
     queryset = Subscription.objects.filter(is_deleted=False).order_by('date_created')
     http_method_names = ('get', 'post', 'put', 'delete')
     permission_classes = (FHIRApiSubscriptionPermissions,)
 
     def perform_destroy(self, instance):
-        self.check_object_owner(self.request.user, instance)
+        if not self.check_if_owner(self.request.user, instance):
+            raise PermissionDenied(
+                detail=self._error_while_deleting % {'msg': 'You are not the owner of this subscription'})
         service = SubscriptionService(self.request.user)
         result = service.delete({'id': instance.id})
         self.check_error_message(result)
@@ -25,8 +27,9 @@ class SubscriptionViewSet(BaseFHIRView, ModelViewSet):
     def check_error_message(result):
         if not result.get('success', False):
             msg = result.get('message', 'Unknown')
-            raise FHIRException(f'Error while deleting a subscription: {msg}')
+            raise APIException(f'Error while deleting a subscription: {msg}')
 
-    def check_object_owner(self, user, instance):
-        if str(user.id).lower() != str(instance.user_created.id).lower():
-            raise FHIRException(self._error_while_saving % {'msg': 'You are not the owner of this subscription'})
+    @staticmethod
+    def check_if_owner(user, instance):
+        return str(user.id).lower() == str(instance.user_created.id).lower()
+
